1f5a6e805dbc8d01bf09cb61a911c729813ebd37
rayhan-rafin <rayhan.rafin2@gmail.com>
2026-08-23 11:48:49 +0600

    upgraded with db skeleton

--- app.py
+++ app.py
@@ -1,69 +1,60 @@
 from flask import Flask, request
+from db import stores, items
+import uuid
+from flask_smorest import abort
+
 
 app = Flask(__name__)
-
-stores = [
-    {
-        "name": "store1",
-        "items": [
-            {
-                "name": "str1_item1",
-                "price": 120
-            }
-        ]
-    }
-]
 
 
 # @app.route("/store", methods=['GET'])
 @app.get("/store")  # shorthand for above, same for post
 # only one method per endpoint, multiple will overwrite (last one remmains)
-def get_stores():
-    return {"stores": stores}
+def get_all_stores():
+    return {"stores": list(stores.values())}
+
+@app.get("/item")
+def get_all_item():
+    return {"items": list(items.values())}
+
+@app.get("/store/<string:store_id>")
+def show_store(store_id):
+    try:
+        return stores[store_id]
+    except KeyError:
+        abort (404 ,message = "store not found in database")
+    
+    
+@app.get("/store/<string:item_id>") #repurposed from all store item to specific item
+def get_item(item_id):
+    try:
+        return items[item_id]
+    except KeyError:
+        abort (404 ,message = "item not found in database")
 
 
+#store creating
 @app.post("/store")
 def create_store():
-    request_data = request.get_json()
-    new_store = {
-        "name": request_data["name"],
-        "items": [
-            {
-                "name": "str2_item1",
-                "price": 150
-            }
-        ]
-    }
-    stores.append(new_store)
-    return new_store, 201
+    store_data = request.get_json()
+    store_id = uuid.uuid4().hex
+    store = {**store_data,"store_id": store_id}
+    stores[store_id]
+    return store, 201
 
 
-@app.post("/store/<string:name>/item")
-def add_item(name):
-    request_data = request.get_json()
-    for target_store in stores:
-        if name == target_store["name"]:
-            new_item = {
-                "name": request_data["name"],
-                "price": request_data["price"]
-            }
-            target_store["items"].append(new_item)
-            return new_item, 201
-    return {"message": "store not in database"}, 404
+@app.post("/item") #item creating
+def add_item():   #not passing any param, how item_data getting values?
+    item_data = request.get_json()
+    if item_data["store_id"] not in stores:
+        return {"message": "store not in database"}, 404
+    
+    item_id = uuid.uuid4().hex
+    item = {**item_data,"item_id":item_id}
+    items[item_id] = item
+    return item,201
 
 
-@app.get("/store/<string:name>")
-def show_store(name):
-    for target_store in stores:
-        if name == target_store["name"]:
-            return target_store
-    return {"message": "store not found in database"}, 404
 
 
-@app.get("/store/<string:name>/item")
-def show_item(name):
-    for target_store in stores:
-        if name == target_store["name"]:
-            # always return=json/dict, client side e code jeno change na korte hoy
-            return {"items": target_store["items"]}
-    return {"message": "store not found"}, 404
+
