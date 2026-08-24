fd4911f0fefddbe0b09f31b9c996e47756e90ff0
rayhan-rafin <rayhan.rafin2@gmail.com>
2026-08-24 18:11:37 +0600

    sqlaclhemy prep

--- schemas.py
+++ schemas.py
@@ -1,15 +1,21 @@
 from marshmallow import Schema,fields
 
-class ItemSchema(Schema):
-    store_id = fields.Str(required=True)
+class PlainItemSchema(Schema):
     item_id = fields.Str(dump_only=True)
     item_name = fields.Str(required=True)
     item_price = fields.Float(required=True)
+    
+class PlainStoreSchema(Schema):
+    store_id = fields.Str(dump_only=True)
+    store_name = fields.Str(required=True)
     
 class UpdateItemSchema(Schema):
     item_name = fields.Str()
     item_price = fields.Float()
     
-class StoreSchema(Schema):
-    store_id = fields.Str(dump_only=True)
-    store_name = fields.Str(required=True)
+class ItemSchema(PlainItemSchema):
+    store_id = fields.Int(required=True,load_only=True)
+    store = fields.Nested(PlainStoreSchema(),dump_only=True)
+    
+class StoreSchema(PlainStoreSchema):
+    items = fields.List(fields.Nested(PlainItemSchema()),dump_only=True)
